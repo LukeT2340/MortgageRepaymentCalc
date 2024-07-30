6a1b0196b4eded9333d028a982619740e504a580
Luke Thompson <lukethompson220@gmail.com>
2024-07-30 13:34:35 +1000

    Repayment mortgage type working

--- script.js
+++ script.js
@@ -1,5 +1,52 @@
-document.getElementById('colorButton').addEventListener('click', function() {
-    const colors = ['#FF5733', '#33FF57', '#3357FF', '#FF33A1', '#33FFF5'];
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    document.body.style.backgroundColor = randomColor;
+
+
+document.getElementById('mortgage-amount').addEventListener('input', function (e) {
+    let value = e.target.value;
+
+    // Remove any non-numeric characters except comma
+    value = value.replace(/[^0-9]/g, '');
+
+    // Format the number with commas
+    value = value.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    e.target.value = value;
+});
+
+document.getElementById('clear-all-button').addEventListener('click', function (e) {
+    document.getElementById('mortgage-amount').value = null
+    document.getElementById('mortgage-term').value = null
+    document.getElementById('interest-rate').value = null
+
+});
+
+document.getElementById('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    // Get and process the values from the form
+    let mortgageAmount = parseFloat(document.getElementById('mortgage-amount').value.replace(/,/g, ''));
+    let mortgageTerm = parseFloat(document.getElementById('mortgage-term').value) * 12; // Convert years to months
+    let annualInterestRate = parseFloat(document.getElementById('interest-rate').value) / 100;
+    let monthlyInterestRate = annualInterestRate / 12;
+
+    console.log(mortgageAmount)
+    console.log(mortgageTerm)
+    console.log(annualInterestRate)
+    console.log(monthlyInterestRate)
+
+    if (!mortgageAmount || !mortgageTerm || !annualInterestRate || !monthlyInterestRate ||mortgageAmount.length === 0 || mortgageAmount.length === 0 || annualInterestRate.length === 0 || monthlyInterestRate === 0) {
+        return
+    }
+
+    // Calculate monthly repayments using the formula
+    let monthlyRepayment = (mortgageAmount * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, mortgageTerm)) / (Math.pow(1 + monthlyInterestRate, mortgageTerm) - 1);
+
+    // Log the result
+    document.getElementById('monthly-repayment').textContent = '£' + monthlyRepayment.toFixed(2);
+
+    // Calculate total repayment
+    let totalRepayment = monthlyRepayment * mortgageTerm;
+
+    document.getElementById('total-repayment').textContent = '£' + (totalRepayment.toFixed(2)).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+    document.getElementById('results-right-side').classList.remove('hidden');
+    document.getElementById('no-results-right-side').classList.add('hidden')
 });
